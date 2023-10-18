61636116d8c97f43d0b67c429c457a28a0aef401
NorthPole0499 <alekseev-timof@mail.ru>
2023-10-18 14:31:31 +0300

    Added product lists and choice of size

--- keyboards.py
+++ keyboards.py
@@ -1,9 +1,44 @@
 from aiogram.types import InlineKeyboardMarkup
 from aiogram.types import InlineKeyboardButton
 
-#register
+# register
 register_button = InlineKeyboardButton(text='Зарегистрироваться', callback_data='register')
 register_inkb = InlineKeyboardMarkup(row_width=1, one_time_keyboard=True).add(register_button)
 
-main_menu_button = InlineKeyboardButton(text='Заказать товар', callback_data='to_main_menu')
-to_main_menu_admin_inkb = InlineKeyboardMarkup(row_width=1, one_time_keyboard=True).add(main_menu_button)
+main_menu_button = InlineKeyboardButton(text='Перейти в главное меню', callback_data='to_main_menu')
+to_main_menu_inkb = InlineKeyboardMarkup(row_width=1, one_time_keyboard=True).add(main_menu_button)
+
+product_list_button = InlineKeyboardButton(text='Список товаров', callback_data='product_list')
+basket_button = InlineKeyboardButton(text='Корзина', callback_data='basket')
+checkout_button = InlineKeyboardButton(text='Оформить заказ', callback_data='checkout')
+main_menu_inkb = InlineKeyboardMarkup(row_width=1, one_time_keyboard=True).add(product_list_button,
+                                                                               basket_button,
+                                                                               checkout_button)
+
+first_item_button = InlineKeyboardButton(text='Первая вещичка', callback_data='first_item')
+second_item_button = InlineKeyboardButton(text='Вторая вещичка', callback_data='second_item')
+third_item_button = InlineKeyboardButton(text='Третья вещичка', callback_data='third_item')
+product_list_inkb = InlineKeyboardMarkup(row_width=1, one_time_keyboard=True).add(first_item_button,
+                                                                                  second_item_button,
+                                                                                  third_item_button,
+                                                                                  main_menu_button)
+
+first_item_S_button = InlineKeyboardButton(text='S', callback_data='first_item_s')
+first_item_M_button = InlineKeyboardButton(text='M', callback_data='first_item_m')
+first_item_L_button = InlineKeyboardButton(text='L', callback_data='first_item_l')
+first_item_XL_button = InlineKeyboardButton(text='XL', callback_data='first_item_xl')
+first_item_inkb = InlineKeyboardMarkup(row_width=1, one_time_keyboard=True).add(first_item_S_button,
+                                                                                first_item_M_button,
+                                                                                first_item_L_button,
+                                                                                first_item_XL_button,
+                                                                                product_list_button)
+
+second_item_add_button = InlineKeyboardButton(text='Добавить в корзину', callback_data='second_item_add')
+second_item_inkb = InlineKeyboardMarkup(row_width=1, one_time_keyboard=True).add(second_item_add_button)
+
+third_item_add_button = InlineKeyboardButton(text='Добавить в корзину', callback_data='third_item_add')
+third_item_inkb = InlineKeyboardMarkup(row_width=1, one_time_keyboard=True).add(third_item_add_button)
+
+
+
+
--- main.py
+++ main.py
@@ -14,12 +14,13 @@ bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 id_of_message = 0
+basket_of_items = []
 
 
 @dp.message_handler(commands=['start'])
 async def register(message: types.Message):
     await message.delete()
-    await message.answer('Привет! Это тг бот ТИНТа. Чтобы начать позьзоваться им, пройди регистрацию.',
+    await message.answer('Привет! Это бот мерча ТИНТа.\nКакой-то дескрипшн',
                          reply_markup=register_inkb)
 
 
@@ -27,7 +28,7 @@ async def register(message: types.Message):
 async def register_call(callback: types.CallbackQuery):
     global id_of_message
     await callback.message.delete_reply_markup()
-    await callback.message.edit_text('Введите ваши ФИО')
+    await callback.message.edit_text('Введите ваши фамилию и имя')
     id_of_message = callback.message.message_id
     await MyDialog.answer.set()
 
@@ -48,7 +49,80 @@ async def process_message(message: types.Message, state: FSMContext):
     await message.delete()
 
     await message.answer('Вы удачно прошли регистрацию',
-                                reply_markup=to_main_menu_admin_inkb)
+                                reply_markup=to_main_menu_inkb)
+
+
+@dp.callback_query_handler(text='to_main_menu')
+async def main_menu_call(callback: types.CallbackQuery):
+    await callback.message.edit_text('Здесь ты можешь выбрать и заказать понравившейся мерч!',
+                                     reply_markup=main_menu_inkb)
+
+
+@dp.callback_query_handler(text='product_list')
+async def main_menu_call(callback: types.CallbackQuery):
+    await callback.message.edit_text('Смотри, что у нас есть!\n'
+                                     'Нажми, чтобы узнать подробную информацию и добавить в заказ!',
+                                     reply_markup=product_list_inkb)
+
+
+@dp.callback_query_handler(text='first_item')
+async def main_menu_call(callback: types.CallbackQuery):
+    await callback.message.edit_text('Это футболочка!\nА это её описание)',
+                                     reply_markup=first_item_inkb)
+
+
+@dp.callback_query_handler(text='first_item_s')
+async def main_menu_call(callback: types.CallbackQuery):
+    basket_of_items.append(("first_item", "S"))
+    await callback.message.edit_text('Готово! Айтем добавлен в заказ.\nЧто еще добавим?',
+                                     reply_markup=product_list_inkb)
+
+
+@dp.callback_query_handler(text='first_item_m')
+async def main_menu_call(callback: types.CallbackQuery):
+    basket_of_items.append(("first_item", "M"))
+    await callback.message.edit_text('Готово! Айтем добавлен в заказ.\nЧто еще добавим?',
+                                     reply_markup=product_list_inkb)
+
+
+@dp.callback_query_handler(text='first_item_l')
+async def main_menu_call(callback: types.CallbackQuery):
+    basket_of_items.append(("first_item", "L"))
+    await callback.message.edit_text('Готово! Айтем добавлен в заказ.\nЧто еще добавим?',
+                                     reply_markup=product_list_inkb)
+
+
+@dp.callback_query_handler(text='first_item_xl')
+async def main_menu_call(callback: types.CallbackQuery):
+    basket_of_items.append(("first_item", "XL"))
+    await callback.message.edit_text('Готово! Айтем добавлен в заказ.\nЧто еще добавим?',
+                                     reply_markup=product_list_inkb)
+
+
+@dp.callback_query_handler(text='second_item')
+async def main_menu_call(callback: types.CallbackQuery):
+    await callback.message.edit_text('Это шопперик!\nА это его описание)',
+                                     reply_markup=second_item_inkb)
+
+
+@dp.callback_query_handler(text='second_item_add')
+async def main_menu_call(callback: types.CallbackQuery):
+    basket_of_items.append(("second_item", "-"))
+    await callback.message.edit_text('Готово! Айтем добавлен в заказ.\nЧто еще добавим?',
+                                     reply_markup=product_list_inkb)
+
+
+@dp.callback_query_handler(text='third_item')
+async def main_menu_call(callback: types.CallbackQuery):
+    await callback.message.edit_text('Это стикерики!\nА это их описание)',
+                                     reply_markup=third_item_inkb)
+
+
+@dp.callback_query_handler(text='third_item_add')
+async def main_menu_call(callback: types.CallbackQuery):
+    basket_of_items.append(("third_item", "-"))
+    await callback.message.edit_text('Готово! Айтем добавлен в заказ.\nЧто еще добавим?',
+                                     reply_markup=product_list_inkb)
 
 
 if __name__ == '__main__':
